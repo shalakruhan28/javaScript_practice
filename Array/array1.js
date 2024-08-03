fe4d859625eb24d367902d87d64a0ac98f46b98d
shalakruhan28 <shalakruhan1996@gmail.com>
2024-08-03 14:33:27 +0600

    random pick from array

---
+++ Array/array1.js
@@ -0,0 +1,25 @@
+console.log("Hi,foxs!")
+
+let messages =["Hey,how are you doing?",
+               "Do you want to go out?"
+]
+
+let newMessages ="Yes,I want to go Sirajganj" 
+
+messages.push(newMessages);
+
+console.log(messages)
+
+messages.pop(newMessages)
+
+console.log(messages)
+
+for(let count= 10; count<101; count +=10){
+  console.log(count)
+}
+
+let randomNumber = Math.floor(Math.random() * 6  ) + 1; 
+console.log(randomNumber)
+
+let flooredNumber = Math.floor(5.999200)
+console.log( flooredNumber)
---
+++ Array/object1.js
@@ -0,0 +1,17 @@
+let Player = {
+   Name:"Messi" ,
+   Age : 37,
+   Nationality :"Argentine",
+   Level: "Highly Pro"
+
+} 
+ console.log(Player.Nationality)
+
+ let airbnb = {
+   CastleName : "PureCastle",
+   isthatopen: true,
+   EntryFee: 5,
+   Castlehas : ["Old Architecture","Infinity View","Clam Weather"]
+ }
+
+  console.log(airbnb.CastleName,  airbnb.EntryFee)
